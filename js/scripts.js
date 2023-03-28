acf54b44dfe1064ab2ba4a550bc0153a77bed668
Cesar <kaesar84@icloud.com>
2023-03-28 18:17:33 +0200

    scroll menu

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,42 @@
+
+/* MENU INICIO */
+
+/*Oculta el menu en base del scroll */
+
+var posInicioScroll = document.documentElement.scrollTop;
+var posFinalScroll = document.documentElement.scrollTop;
+
+
+window.onscroll = function() {estadoMenu()};
+ 
+function estadoMenu(){
+  var posActualScroll = document.documentElement.scrollTop;
+
+  if(posInicioScroll>posActualScroll){
+    // subiendo pantalla - muestra menu
+    //document.getElementById("menu").style.display="flex";
+    document.getElementById("menuBar").style.top= "0";
+  
+
+  }else{
+    //bajando - oculta menu
+   // document.getElementById("menu").style.display="none";
+   document.getElementById("menuBar").style.top= "-200px";
+  }
+  posInicioScroll=posActualScroll;
+
+}
+
+    
+
+  
+
+  
+
+
+
+
+
 /* INICIO Formulario */
 
 /*Realiza la función de mostrar el modal, así como bloquear el scroll. 
@@ -31,7 +70,7 @@ contactarAbrir = () => {
       mensaje =
         "Hola " +
         nombre +
-        "<br>Contactaremos en tu teléfono " +
+        ",<br>contactaremos en tu teléfono " +
         telefono +
         "<br>Gracias por tu confianza";
       document.getElementById("mensajeFormulario").innerHTML = mensaje;
