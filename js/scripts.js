37dee09f14bbefaa6e20d050f8e0fff0e6d34035
Cesar <kaesar84@icloud.com>
2023-04-09 00:02:09 +0200

    update

--- js/scripts.js
+++ js/scripts.js
@@ -328,3 +328,18 @@ activarTooltip = () =>{
 
 
 
+function filterImages(category) {
+  const imagenesFiltro = document.getElementsByClassName('imgfiltro');
+  
+  for (let i = 0; i < imagenesFiltro.length; i++) {
+    const imagenesFiltroS = imagenesFiltro[i];
+   
+    if (category === 'todos') {
+      imagenesFiltroS.style.display = 'flex';
+    } else if (imagenesFiltroS.classList.contains(category)) {
+      imagenesFiltroS.style.display = 'flex';
+    } else {
+      imagenesFiltroS.style.display = 'none';
+    }
+  }
+}
